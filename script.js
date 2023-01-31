edea863c455487e5295bff4b1bc8d31ccd6aa9ec
melissa-studi <melissa_bazir@yahoo.fr>
2023-01-31 07:11:33 +0100

    Initial

---
+++ script.js
@@ -0,0 +1,27 @@
+/*  1 2
+    2 1
+ */
+let jeuTableau;
+let allCards = document.querySelectorAll(".card");
+
+allCards.forEach((card) => {
+  card.addEventListener("click", function () {
+    if (card.classList.contains("hidden")) {
+      card.classList.remove("hidden");
+    } else {
+      card.classList.add("hidden");
+    }
+  });
+});
+
+function generateGameArray(x, y) {
+  let Tableau = new Array(x);
+  for (let i = 0; i < Tableau.length; i++) {
+    Tableau[i] = new Array(y);
+  }
+  jeuTableau = Tableau;
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
