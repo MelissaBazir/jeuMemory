9e9d61124c9f077155aa640a1b087dca0733cd78
melissa-studi <melissa_bazir@yahoo.fr>
2023-01-31 16:00:50 +0100

    Début du jeu

--- script.js
+++ script.js
@@ -3,25 +3,49 @@
  */
 let jeuTableau;
 let allCards = document.querySelectorAll(".card");
+let cptClickCurrent = 0;
+let dataImageShowed;
 
 allCards.forEach((card) => {
   card.addEventListener("click", function () {
-    if (card.classList.contains("hidden")) {
-      card.classList.remove("hidden");
-    } else {
-      card.classList.add("hidden");
-    }
+    playGame(card);
   });
 });
 
-function generateGameArray(x, y) {
-  let Tableau = new Array(x);
-  for (let i = 0; i < Tableau.length; i++) {
-    Tableau[i] = new Array(y);
-  }
-  jeuTableau = Tableau;
-}
+function playGame(card) {
+  cptClickCurrent++;
+  card.classList.remove("hidden");
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
+  if (cptClickCurrent === 1) {
+    //Premier click, je cache les images trouvées avant
+    allCards.forEach((card) => {
+      if (card.classList.contains("finded")) {
+        //c'est une carte trouvée
+      } else {
+        // pas trouvée, il faut qu'elle soit masquée
+        card.classList.add("hidden");
+      }
+    });
+    // j'affiche la carte sur laquelle je viens de cliquer
+    card.classList.remove("hidden");
+    // je stocke la réponse derrière la carte et je la retourne
+    dataImageShowed = card.dataset.image;
+  } else if (cptClickCurrent === 2) {
+    //deuxième fois, vérifier si les cartes sont identiques
+    card.classList.remove("hidden");
+    if (dataImageShowed === card.dataset.image) {
+      allCards.forEach((card) => {
+        if (card.classList.contains("hidden")) {
+          //c'est une carte cachée
+        } else {
+          card.classList.add("finded");
+        }
+      });
+    }
+    cptClickCurrent = 0;
+    dataImageShowed = "";
+
+    // Compter les cartes qui n'ont pas la classe "finded"
+    // si = 0; alors le jeu est fini
+  }
 }
